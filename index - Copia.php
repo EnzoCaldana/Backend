15527ff520f941017ebce6d61974621fe92efe31
EnzoCaldana <enzo.caldana.senai404@gmail.com>
2024-08-02 10:41:30 -0300

    Add files via upload

---
+++ index - Copia.php
@@ -0,0 +1,21 @@
+<?php 
+// definicao das variaveis 
+$nome = 'Enzo ';
+$escola = 'SESI Cerquilho <br>';
+$cidade = 'Cerquilho <br>';
+$CE = 428;
+$curso = 'Desenvolvimento de Sistemas <br>';
+$media = 9.5;
+
+echo $nome;
+echo $escola;
+echo $CE;
+echo $curso;
+echo $media;
+
+echo "Ola $nome, Bem vindo ao SESI $CE de $cidade <br>";
+echo 'Ola mundo <br> '; 
+echo 'Bem Vindo ao Sesi/Senai <br>';  
+
+
+?>
